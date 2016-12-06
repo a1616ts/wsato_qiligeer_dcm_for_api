464126c2a4ac3faafe9cafb4ed8c0efff10b217f
a1616ts <a1616ts@aiit.ac.jp>
2016-12-06 20:01:45 +0900

    Modify.

--- service.py
+++ service.py
@@ -4,7 +4,20 @@ import dataset
 import uuid
 import traceback
 import pymysql
+import logging
+import logging.handlers
 pymysql.install_as_MySQLdb()
+
+
+logger = logging.getLogger('wsato_qiligeer_dcm_for_api')
+logger.setLevel(logging.WARNING)
+handler = logging.handlers.TimedRotatingFileHandler(
+    filename = '/var/log/wsato_qiligeer/wsato_qiligeer_dcm_for_api.log',
+    when = 'D'
+    )
+handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+logger.addHandler(handler)
+
 
 credentials = pika.PlainCredentials('server1_dcm', '8nfdsS12gaf')
 connection  = pika.BlockingConnection(pika.ConnectionParameters(
@@ -17,51 +30,66 @@ channel1.queue_declare(queue = 'from_api_to_middleware', durable = True)
 channel2 = connection.channel()
 channel2.queue_declare(queue = 'from_middleware_to_agent', durable = True)
 
+
 def from_api_to_middleware_callback(ch, method, properties, body):
     decoded_json = json.loads(body.decode('utf-8'))
     user_id = decoded_json['user_id']
     display_name = decoded_json['name']
     operation = decoded_json['op']
 
-    db = dataset.connect('mysql://dcm_user:dcmUser@1115@127.0.0.1/wsato_qiligeer')
+    db = dataset.connect('mysql://dcm_user:dcmUser@1115@localhost/wsato_qiligeer')
+
     if operation == 'create':
         # Select server
-        vc_servers_table = db['vc_servers']
-        results = vc_servers_table.find_one(order_by = '-free_size_gb')
         server_id = results['id']
         server_name = results['name']
+        os = results['os']
         free_size_gb = int(results['free_size_gb'])
         size = int(decoded_json['size'])
         ram = int(decoded_json['ram'])
         vcpus = int(decoded_json['vcpus'])
-        # TODO add os
+
+
+        target_server = None
+        t_size = 0
+        t_core = 0
+        t_ram = 0
+        vc_servers_table = db['vc_servers']
+        for server in vc_servers_table.find:
+            t_size = int(vc_server['free_size_gb']) - size
+            t_core = int(vc_server['free_cpu_core']) - vcpus
+            t_ram = int(vc_server['free_ram_byte']) - ram
+            if 0 < t_size and  0 < t_core and 0 < t_ram:
+                target_server = server
 
         # Check free space
-        if free_size_gb < size:
-            return # TODO logger
+        if target_server == None:
+            return
 
         # Check name
         domains_table = db['domains']
         result = domains_table.find_one(
             user_id  = user_id,
             name     = display_name)
+
         if result != None:
-            # TODO logger
+            logger.eror('Can not create vm because of name duplication.')
             return
 
-        # Create uniquename and mapping
-        domain_name = uuid.uuid4().hex
+        # Create unique name
+        name = uuid.uuid4().hex
 
         db.begin()
         try:
-            # Create domains record.
-            domain_id = domains_table.insert(dict(name = domain_name, display_name = display_name, size = size, vcpus = vcpus, ram = ram,  user_id = user_id, server_id = server_id))
+            # Create domains record
+            domain_id = domains_table.insert(dict(name = name, display_name = display_name, size = size, vcpus = vcpus, ram = ram,  user_id = user_id, server_id = server_id))
             # Update vc_server
-            vc_servers_table.update(dict(id = server_id, free_size_gb = free_size_gb - size), ['id'])
+            vc_servers_table.update(dict(id = server_id, free_size_gb = t_size, free_cpu_core = t_core, free_ram_byte = t_ram), ['id'])
             db.commit()
         except:
-            print(traceback.format_exc())
             db.rollback()
+            logger.eror('Insert or Update failed due to data error.')
+            logger.eror(traceback.format_exc())
             return
 
         # Select Vhosts
@@ -72,9 +100,9 @@ def from_api_to_middleware_callback(ch, method, properties, body):
 
         enqueue_message = {
             'op'    : operation,
-            'name'  : domain_name,
+            'name'  : name,
             'size'  : size,
-            'os'    : 'centos7',# decoded_json['os'],
+            'os'    : decoded_json['os'],
             'ram'   : decoded_json['ram'],
             'vcpus' : decoded_json['vcpus']
         }
@@ -88,20 +116,17 @@ def from_api_to_middleware_callback(ch, method, properties, body):
         domains_table = db['domains']
         result = domains_table.find_one(
             user_id  = user_id,
-            name     = vm_name)
+            name     = display_name)
         if result == None:
-            print('bad permission')
-            # TODO Write status
+            logger.eror('Can not operation vm because of it is not exists.')
             return
 
-        domain_id    = result['domain_id']
-        domain_table = db['domains']
-        result       = domain_table.find_one(domain_id = domain_id)
+        domain_id = result['id']
+        result    = domain_table.find_one(domain_id = domain_id)
 
-        # TODO Check operation
         enqueue_message = {
             'op'  : operation,
-            'name' : result['domain_name']
+            'name' : display_name
         }
 
         server_table = db['servers']
